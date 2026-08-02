48920d8dd2429b7f06707b7d75dfccb936304932
leticiaparreiras <leticia.x.parreiras@gmail.com>
2026-08-02 12:24:37 -0300

    feat(AppModule) Configuração do Mongoose com uso de ConfigService

--- Api/src/app.module.ts
+++ Api/src/app.module.ts
@@ -5,16 +5,23 @@ import { ImagesModule } from './images/images.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { FollowModule } from './follow/follow.module';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [ImagesModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/image'),
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.getOrThrow<string>('DB_URL'),
+      }),
+    }),
+    ImagesModule,
     UserModule,
     AuthModule,
-    ConfigModule.forRoot({ isGlobal: true }),
     FollowModule,
     PostsModule,
   ],
